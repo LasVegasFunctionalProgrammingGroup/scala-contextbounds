9b7835179a626adb7e2c65000850a44c839b9563
Andy Wortman <andy.wortman@originate.com>
2014-11-08 13:59:16 -0800

    Descriptions

--- src/main/scala/Defaults.scala
+++ src/main/scala/Defaults.scala
@@ -7,10 +7,13 @@ package object defaults {
     def value: T
   }
 
+  // this adds in a .getOrDefault on Option so
+  // any Option[T] with a Default[T] in scope can be defaulted if it's None
   implicit class DefaultEvaluator[T : Default](t: Option[T]) {
     def getOrDefault: T = t.getOrElse(implicitly[Default[T]].value)
   }
 
+  // helper function to create a new Default from any value
   private def default[T](x: T) = new Default[T] {
     def value = x
   }
@@ -20,7 +23,15 @@ package object defaults {
       def value = 0
     }
 
+    // Since tese are both Default[String] the compiler will
+    // give an error if Default._ is imported and a Default[String]
+    // is needed, due to ambiguity on which to use.
+    //
+    // HOWEVER: If only one is imported, like `import Default.defaultName`
+    // the compiler shouldn't throw an error; only one implicit Defaul[T]
+    // is in scope
     implicit val defaultName = default("John Doe")
+    implicit val defaultName2 = default("DefaultName2")
 
     implicit val defaultHost = default(Host("localhost", "127.0.0.1"))
   }
--- src/main/scala/example.scala
+++ src/main/scala/example.scala
@@ -4,18 +4,26 @@ import lvfpug.contextbounds.defaults._
 import lvfpug.contextbounds.defaults.DefaultStreams._
 import java.io.PrintStream
 
+/*
+  Look at the Default trait and helpers set up in Defaults.scala first!
+*/
+
 object ContextBoundExamples extends App {
   def log[T <: PrintStream : Default](thing: Any, output: Option[T] = None) =
     output.getOrDefault.println(thing)
 
   // equivalent to
   // def log[T <: PrintStream](thing: Any, output: Option[T] = None)(implicit ev: Default[T]) = /* body */
+  // note the rewrite of : Default as an implicit argument.
+  // Since context bounds work through implicits, the name of the argument
+  // doesn't matter, and the name of the implicit doesn't matter either.
+  //  Only types are examined by the compiler!
 
-  log("Hello!")
-  log("To a file", Some(new PrintStream("logfile")))
+  log("Hello!")  // Since output defaults to None, we can leave it off. Uses the default then!
+  log("To a file", Some(new PrintStream("logfile"))) // or explicitly provide an output
 
 
-
+  // Since Default takes any T, we can use our own case classes
   def pingHost[T <: Host : Default](target: Option[Host] = None) = {
     val host = target.getOrDefault
     log(s"Pinging $host")
@@ -25,7 +33,7 @@ object ContextBoundExamples extends App {
   //pingHost()
 
 
-
+  // And we can add a helper for things to read a bit more cleanly (IMO)
   def withDefaulted[T : Default, U](t: Option[T])(f: T => U): U = f(t.getOrDefault)
 
   def betterPingHost[T <: Host : Default](target: Option[Host] = None) =
